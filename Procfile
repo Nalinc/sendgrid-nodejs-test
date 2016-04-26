ef6ab40842a843df2bfeac4ad5f902440b68bc0c
nalinc <nalin.chhibber@gmail.com>
2016-04-27 02:13:28 +0530

    firsrt commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node smtp-nodejs-example.js
--- smtp-nodejs-example.js
+++ smtp-nodejs-example.js
@@ -1,8 +1,18 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+
+var app = express();
+
 var dotenv = require('dotenv');
 dotenv.load();
 
 var nodemailer = require('nodemailer');
 var smtpapi    = require('smtpapi');
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname))
+app.use(express.static(__dirname + '/demo'))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 var sendgrid_username   = process.env.SENDGRID_USERNAME;
 var sendgrid_password   = process.env.SENDGRID_PASSWORD;
@@ -27,32 +37,52 @@ var settings  = {
 };
 var smtpTransport = nodemailer.createTransport(settings);
 
-var mailOptions = {
-  from:     to,
-  to:       to,
-  subject:  "[smtp-nodejs-example] Owl",
-  text:     "%how% are you doing?",
-  html:     "<strong>%how% are you doing?</strong>",
-  attachments: { 
-    path: './gif.gif',
-    filename: 'owl.gif' 
-  },
-  headers:  headers
-}
 
-smtpTransport.sendMail(mailOptions, function(error, response) {
-  smtpTransport.close();
 
-  if (error) { 
-    console.log(error);
-  } 
-  else {
-      console.log("Message sent: ");
-      console.log("-------------------------");
-      console.log("Accepted: " + response.accepted);
-      console.log("Rejected: " + response.rejected);
-      console.log("Response: " + response.response);
-      console.log("Envelope: " + JSON.stringify(response.envelope));
-      console.log("MessageId: " + response.messageId);
-  }
+app.get('/', function(request, response) {
+  response.render('index.html')
+})
+
+app.post('/sendmail', function(request, response){
+
+    var mailOptions = {
+      from:     request.body.from,
+      to:       request.body.to,
+      subject:  request.body.subject,
+      text:     request.body.content,
+      html:     "<strong>"+request.body.content+"</strong>",
+      attachments: { 
+        path: './gif.gif',
+        filename: 'owl.gif' 
+      },
+      headers:  headers
+    }
+
+    smtpTransport.sendMail(mailOptions, function(error, res) {
+      smtpTransport.close();
+
+      if (error) { 
+        console.log(error);
+        response.send(error);
+      } 
+      else {
+          var obj = {
+            "msg":"Message sent",
+            "Accepted":res.accepted,
+            "Rejected":res.rejected,
+            "Response":res.response,
+            "Envelope":JSON.stringify(res.envelope),
+            "MessageId":res.messageId
+          }
+          console.log(obj);
+          response.send(obj);
+      }
+    });
+
+
 });
+
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
